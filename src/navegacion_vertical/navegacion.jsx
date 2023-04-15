a8319470de91822aa58002fa95df4133aade4ba9
Liderman Giraldo <lidermangiraldo@hotmail.com>
2023-04-15 16:08:18 -0500

    Se agregaron mas tabs a la navegación de empleados

--- src/navegacion_vertical/navegacion.jsx
+++ src/navegacion_vertical/navegacion.jsx
@@ -5,28 +5,27 @@ import './menuVertical.css'
 import imagesBarNav from './script';
 
 const CompNavegacionVertical = () => {
-    const [isExpanded, setIsExpanded] = useState(false);
-
+    
     const toggleSidebar = () => {
         document.body.classList.toggle("open");
     };
 
-    const [activeSubNav, setActiveSubNav] = useState('');
+    const [isExpanded, setIsExpanded] = useState(false);
+    const [activeNavItem, setActiveNavItem] = useState(null);
 
     const resetSidebar = () => {
-        setActiveSubNav('');
+        setActiveNavItem(null);
     };
 
-    const handleHeaderClicked = (subNav) => {
-        setIsExpanded(!isExpanded);
-        if (subNav === activeSubNav) {
-        resetSidebar();
-        return;
+    const handleHeaderClicked = (navItem) => {
+        if (activeNavItem === navItem) {
+            resetSidebar();
+        } else {
+            setActiveNavItem(navItem);
         }
-
-        resetSidebar();
-        setActiveSubNav(subNav);
+        setIsExpanded(!isExpanded);
     };
+
 
     return (
         <aside>
@@ -46,12 +45,33 @@ const CompNavegacionVertical = () => {
             </div>
             <div class="cmp-sidebar-nav">
                 <nav>
-                    <NavLink to="/empleados" className="cmp-sidebar-custom-link">
-                        <button type="button" to="/empleados">
+                    <NavLink className="cmp-sidebar-custom-link" onClick={() => handleHeaderClicked('empleados')}>
+                        <button type="button">
                             <img src={ imagesBarNav.iconTest } />
                             <span className="cmp-sidebar-text-item">Empleados</span>
+                            <span className={`material-symbols-outlined cmp-sidebar-text-item ${activeNavItem === 'empleados' ? 'active' : ''}`}>
+                                expand_more
+                            </span>
                         </button>
                     </NavLink>
+
+                    <div
+                        className={`subnav ${activeNavItem === 'empleados' ? 'active' : ''}`}
+                        style={{ height: activeNavItem === 'empleados' ? '120px' : '0' }}>
+                        <div className="cmp-sidebar-subnav-inner">
+                            <NavLink to="/pedidos-activos" className="cmp-sidebar-custom-link">
+                                <button type="button">
+                                    <span className="cmp-sidebar-text-item">Ver empleados</span>
+                                </button>
+                            </NavLink>
+
+                            <NavLink to="/pedidos-finalizados" className="cmp-sidebar-custom-link">
+                                <button type="button">
+                                    <span className="cmp-sidebar-text-item">Crear empleado</span>
+                                </button>
+                            </NavLink>
+                        </div>
+                    </div>
 
                     <NavLink to="/clientes" className="cmp-sidebar-custom-link">
                         <button type="button">
@@ -60,19 +80,19 @@ const CompNavegacionVertical = () => {
                         </button>
                     </NavLink>
 
-                    <NavLink className="cmp-sidebar-custom-link" onClick={() => handleHeaderClicked('pedidos')}>    
+                    <NavLink className="cmp-sidebar-custom-link" onClick={() => handleHeaderClicked('pedidos')}>
                         <button type="button">
-                            <img src={ imagesBarNav.iconTest } />
+                        <img src={ imagesBarNav.iconTest } />
                             <span className="cmp-sidebar-text-item">Pedidos</span>
-                            <span className={`material-symbols-outlined cmp-sidebar-text-item ${isExpanded ? 'active' : ''}`}>
+                            <span className={`material-symbols-outlined cmp-sidebar-text-item ${activeNavItem === 'pedidos' ? 'active' : ''}`}>
                                 expand_more
                             </span>
                         </button>
                     </NavLink>
 
-                    <div 
-                        id="pedidos" className={`subnav ${activeSubNav === 'pedidos' ? 'active' : ''}`}
-                        style={{ height: activeSubNav === 'pedidos' ? '120px' : '0' }}>
+                    <div
+                        className={`subnav ${activeNavItem === 'pedidos' ? 'active' : ''}`}
+                        style={{ height: activeNavItem === 'pedidos' ? '120px' : '0' }}>
                         <div className="cmp-sidebar-subnav-inner">
                             <NavLink to="/pedidos-activos" className="cmp-sidebar-custom-link">
                                 <button type="button">
@@ -80,9 +100,9 @@ const CompNavegacionVertical = () => {
                                 </button>
                             </NavLink>
 
-                            <NavLink to="/pedidos-finalizados" className="cmp-sidebar-custom-link">
+                            <NavLink to="/pedidos-activos" className="cmp-sidebar-custom-link">
                                 <button type="button">
-                                    <span className="cmp-sidebar-text-item">Pedidos finalizados</span>
+                                    <span className="cmp-sidebar-text-item">Pedidos activos</span>
                                 </button>
                             </NavLink>
                         </div>
