74f22e7a63fbe518482706f910eeb65244d14be0
John Angel Henao <john.alvarez@correounivalle.edu.co>
2023-04-22 12:32:48 -0500

    Ajustes componente cliente

--- src/employees/CompShowEmployees.jsx
+++ src/employees/CompShowEmployees.jsx
@@ -17,21 +17,41 @@ const CompShowUsuarios = () => {
     const [searchTerm, setSearchTerm] = useState('');
     const [filteredUsuarios, setFilteredUsuarios] = useState([]);
 
+
     useEffect(() => {
-        getUsuarios()
-    }, [])
+        let token = localStorage.getItem('token');
+        if (!token) {
+            // Si el token no está presente, redirige al usuario a la página de inicio de sesión
+            window.location.href = '/';
+        } else {
+            // Si el token está presente, llama a la función getUsuarios para obtener la información del empleado
+            getUsuarios(token).catch((error) => { });
+        }
+    }, []);
 
+    
 
+    const getUsuarios = async (token) => {
+        
+        // Verificar si el token es válido antes de continuar
+        if (!token) {
+            return;
+        }
 
-    console.log(usuarios)
-
-    //procedimineto para mostrar todos los empleados
-    const getUsuarios = async () => {
-        const res = await axios.get(URI)
-        setUsuarios(res.data)
-        setLoading(false) 
-        console.log("Informacion empleado", res)
-    }
+        try {
+            const res = await axios.get(URI, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            setUsuarios(res.data);
+            setLoading(false);
+            console.log("Informacion empleado", res);
+        } catch (error) {
+            // Si hay un error al obtener la información del empleado, redirige al usuario a la página de inicio de sesión
+            window.location.href = '/';
+        }
+    };
 
     //Procedimiento para realizar busqueda de empleados
     useEffect(() => {
@@ -71,69 +91,37 @@ const CompShowUsuarios = () => {
                         <Link className="btn-add-new-link" to="/create-employees"><button type="submit" className="btn-add-new"><img src={imagesEmployees.iconAdd} alt="lupa" /><p>Agregar empleado</p></button></Link>
                     </div>
                 </div>
-                
+
                 <div className="table-empleados-container">
                     <table className='table-empleados'>
-                            <thead className='table-primary'>
-                                <tr>
-                                    <th>Nombre</th>
-                                    <th>Apellido</th>
-                                    <th>Teléfono</th>
-                                    <th>Especialidad</th>
-                                    <th>Operaciones</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {filteredUsuarios.map((usuario) => (
-                                    <tr key={usuario.idcedula}>
-                                        <td> {usuario.nombre} </td>
-                                        <td> {usuario.apellido} </td>
-                                        <td> {usuario.telefono} </td>
-                                        <td> {usuario.especialidad} </td>
-                                        <td className="colum-table-actions">
-                                            <Link to={`/edit/${usuario.idcedula}`} className='btn-action'><i className="fas fa-edit "></i></Link>
-                                            <Link onClick={() => deleteUsuarios(usuario.idcedula)} className='btn-action'><i className="fas fa-trash-alt"></i></Link>
-                                            <Link to={`/registro-horas-empleado/${usuario.idcedula}`} className="btn-action"><i className="fas fa-dollar-sign"></i></Link>
-                                            <Link to={`/ingresar_fecha/${usuario.idcedula}`} className="btn-action"><i className="fas fa-dollar-sign"></i></Link>
-                                        </td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                </div>
-            </div>
-            
-            {/* <div className='cmp-container-show-employees'>
-                <div className='col'>
-                    {<Link to="/create-employees" className='btn btn-primary mt-2 mb-2'><i className="fas fa-plus"></i></Link>}
-                    <table className='table'>
                         <thead className='table-primary'>
                             <tr>
                                 <th>Nombre</th>
                                 <th>Apellido</th>
-                                <th>Cedula</th>
-                                <th>Telefono</th>
+                                <th>Teléfono</th>
+                                <th>Especialidad</th>
+                                <th>Operaciones</th>
                             </tr>
                         </thead>
                         <tbody>
-                            {usuarios.map((usuario) => (
+                            {filteredUsuarios.map((usuario) => (
                                 <tr key={usuario.idcedula}>
                                     <td> {usuario.nombre} </td>
                                     <td> {usuario.apellido} </td>
-                                    <td> {usuario.idcedula} </td>
                                     <td> {usuario.telefono} </td>
-                                    <td>
-                                        <Link to={`/edit/${usuario.idcedula}`} className='btn btn-info'><i className="fas fa-edit"></i></Link>
-                                        <button onClick={() => deleteUsuarios(usuario.idcedula)} className='btn btn-danger'><i className="fas fa-trash-alt"></i></button>
-                                        <Link to={`/registro-horas-empleado/${usuario.idcedula}`} className="btn btn-success"><i className="fas fa-dollar-sign"></i></Link>
-                                        <Link to={`/ingresar_fecha/${usuario.idcedula}`} className="btn btn-success"><i className="fas fa-dollar-sign"></i></Link>
+                                    <td> {usuario.especialidad} </td>
+                                    <td className="colum-table-actions">
+                                        <Link to={`/edit/${usuario.idcedula}`} className='btn-action'><i className="fas fa-edit "></i></Link>
+                                        <Link onClick={() => deleteUsuarios(usuario.idcedula)} className='btn-action'><i className="fas fa-trash-alt"></i></Link>
+                                        <Link to={`/registro-horas-empleado/${usuario.idcedula}`} className="btn-action"><i className="fas fa-dollar-sign"></i></Link>
+                                        <Link to={`/ingresar_fecha/${usuario.idcedula}`} className="btn-action"><i className="fas fa-dollar-sign"></i></Link>
                                     </td>
                                 </tr>
                             ))}
                         </tbody>
                     </table>
                 </div>
-            </div> */}
+            </div>
         </div>
 
     )
--- src/home_principal/homePrincipal.jsx
+++ src/home_principal/homePrincipal.jsx
@@ -1,93 +1,3 @@
-/* import { dividerClasses } from "@mui/material";
-import axios from "axios";
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import './homePrincipal.css'
-import { Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button } from "@mui/material";
-
-
-
-
-const CompHomePrincipal = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [usernameError, setUsernameError] = useState('');
-    const [passwordError, setPasswordError] = useState('');
-    const [open, setOpen] = useState(false);
-
-    const handleLogin = () => {
-
-
-        if (!username) {
-            setUsernameError('El campo de usuario es obligatorio');
-        } else {
-            setUsernameError('');
-        }
-
-        if (!password) {
-            setPasswordError('El campo de contraseña es obligatorio');
-        } else {
-            setPasswordError('');
-        }
-
-        if (!username || !password) {
-            return;
-        }
-
-        if (username === "admin" && password === "admin") {
-            // redireccionar a otra ruta
-            window.location.href = "/homeAdministrador";
-        } else {
-            setOpen(true);
-        }
-    };
-
-    return (
-        <div className="container-login-markey">
-            <div className="login-container">
-                <div className="login-card">
-                    <h2>Welcome</h2>
-                    <div className="cmp-container-input-login">
-                        <input
-                            type="text"
-                            id="username"
-                            placeholder="Usuario"
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                        />
-                        {usernameError && <div className="error">{usernameError}</div>}
-
-                        <input
-                            type="password"
-                            id="password"
-                            placeholder="Contraseña"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                        {passwordError && <div className="error">{passwordError}</div>}
-
-                        <button onClick={handleLogin} className="btn-iniciar-sesion">Iniciar sesión</button>
-                    </div>
-                </div>
-            </div>
-
-            <Dialog open={open} onClose={() => setOpen(false)}>
-                <DialogTitle>Error de inicio de sesión</DialogTitle>
-                <DialogContent>
-                    <DialogContentText>
-                        Usuario o contraseña incorrectos.
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={() => setOpen(false)}>Cerrar</Button>
-                </DialogActions>
-            </Dialog>
-        </div>
-    );
-};
-
-export default CompHomePrincipal; */
-
 import { useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
@@ -99,7 +9,7 @@ import logo from '../assets/logo.png';
 import bgWaves from '../assets/wave.png';
 import imgDecor from '../assets/im1.svg';
 
-const CompHomePrincipal = () => {
+const CompHomePrincipal = ({token}) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [usernameError, setUsernameError] = useState('');
@@ -126,8 +36,14 @@ const CompHomePrincipal = () => {
     try {
       const response = await axios.post('http://localhost:8000/login/', { username, password });
       if (response.status === 200) {
+        // Guardar el token en el almacenamiento local del navegador
+        localStorage.setItem('token', response.data.token);
+    
+        // Configurar el encabezado de autorización para todas las solicitudes
+        axios.defaults.headers.common['Authorization'] = `Bearer ${response.data.token}`;
+        console.log(response)
         // redireccionar a otra ruta
-        window.location.href = "/homeAdministrador";
+         window.location.href = "/homeAdministrador";
       } else {
         setOpen(true);
       }
@@ -179,32 +95,6 @@ const CompHomePrincipal = () => {
           </div>
         </div>
       </div>
-      {/* <div className="login-container">
-        <div className="login-card">
-          <h2>Welcome</h2>
-          <div className="cmp-container-input-login">
-            <input
-              type="text"
-              id="username"
-              placeholder="Usuario"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            {usernameError && <div className="error">{usernameError}</div>}
-
-            <input
-              type="password"
-              id="password"
-              placeholder="Contraseña"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            {passwordError && <div className="error">{passwordError}</div>}
-
-            <button onClick={handleLogin} className="btn-iniciar-sesion">Iniciar sesión</button>
-          </div>
-        </div>
-      </div> */}
 
       <Dialog open={open} onClose={() => setOpen(false)}>
         <DialogTitle>Error de inicio de sesión</DialogTitle>
