8a2b534c9a49183d66f108c54ec625427838a464
John Angel Henao <john.alvarez@correounivalle.edu.co>
2023-05-23 16:22:13 -0500

    Implementacion estado de carga

--- src/home_administrador/homePrincipalAdministrador.jsx
+++ src/home_administrador/homePrincipalAdministrador.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import './home.css'
 import BarsChart from "./BarsChart";
 import PiesChart from "./PiesChart";
-
+import { FaCircleNotch } from 'react-icons/fa';
 
 import CompHeader from "../header/header";
 import CompNavegacionVertical from "../navegacion_vertical/navegacion";
@@ -28,160 +28,169 @@ if (currentUrl.includes('localhost')) {
 
 
 const CompHomePrincipalAdministrador = () => {
+  const [pedidos, setPedidos] = useState([])
+  const [pedidosFinalizados, setPedidosFinalizados] = useState([])
+  const [totalItemsPedidos, setItemsPedidos] = useState([])
+  const [empleados, setEmpleados] = useState([])
+  const [horasEmpleados, setHorasEmpleados] = useState([])
+  const [totalPagarEmpleados, setTotalPagarEmpleados] = useState([])
+  const [isLoading, setLoading] = useState(false);
+  const [totalPrecio, setTotalPrecio] = useState(0);
+  const [nombre_admin, setNombreAdmin] = useState('');
 
-    const [pedidos, setPedidos] = useState([])
-    const [pedidosFinalizados, setPedidosFinalizados] = useState([])
-    const [totalItemsPedidos, setItemsPedidos] = useState([])
-    const [empleados, setEmpleados] = useState([])
-    const [horasEmpleados, setHorasEmpleados] = useState([])
-    const [totalPagarEmpleados, setTotalPagarEmpleados] = useState([])
-    const [, setLoading] = useState(false);
-    const [totalPrecio, setTotalPrecio] = useState(0);
-    const [nombre_admin, setNombreAdmin] = useState('');
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setLoading(true);
 
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                setLoading(true);
-
-                let token = localStorage.getItem('token');
-                if (!token) {
-                  // Si el token no está presente, redirige al usuario a la página de inicio de sesión
-                  window.location.href = '/';
-                  return;
-                }
-        
-
-                const [pedidosResponse, pedidosFinalizadosResponse, itemsPedidosResponse, empleadosResponse, horasEmpleadosResponse, configuracionesResponse] = await Promise.all([
-                    axios.get(URI, {
-                      headers: {
-                        Authorization: `Bearer ${token}`,
-                      },
-                    }),
-                    axios.get(URI_PEDIDOS_FINALIZADOS, {
-                      headers: {
-                        Authorization: `Bearer ${token}`,
-                      },
-                    }),
-                    axios.get(URI_ITEMS_PEDIDOS, {
-                      headers: {
-                        Authorization: `Bearer ${token}`,
-                      },
-                    }),
-                    axios.get(URI_EMPLEADOS_REGISTRADOS, {
-                      headers: {
-                        Authorization: `Bearer ${token}`,
-                      },
-                    }),
-                    axios.get(URI_HORAS_REGISTRADAS, {
-                      headers: {
-                        Authorization: `Bearer ${token}`,
-                      },
-                    }),
-                    axios.get(URI_CONFIGURACIONES, {
-                      headers: {
-                        Authorization: `Bearer ${token}`,
-                      },
-                    }),
-                  ]);
-
-                setPedidos(pedidosResponse.data);
-                setPedidosFinalizados(pedidosFinalizadosResponse.data);
-                setTotalPrecio(
-                    pedidosFinalizadosResponse.data.reduce(
-                        (accumulator, pedido) => accumulator + parseFloat(pedido.precio_pedido),
-                        0
-                    )
-                );
-                setItemsPedidos(
-                    itemsPedidosResponse.data.reduce(
-                        (accumulator, item) => accumulator + parseFloat(item.cantidad),
-                        0
-                    )
-                );
-
-                setEmpleados(empleadosResponse.data);
-                setHorasEmpleados(horasEmpleadosResponse.data.reduce(
-                    (accumulator, hora) => accumulator + parseFloat(hora.total_horas),
-                    0
-                ));
-
-                setTotalPagarEmpleados(horasEmpleadosResponse.data.reduce(
-                    (accumulator, hora) => accumulator + parseFloat(hora.total_pagar),
-                    0
-                ));
-
-                setNombreAdmin(configuracionesResponse.data[0].nombre_admin);
-            } catch (error) {
-                console.error(error);
-            } finally {
-                setLoading(false);
-            }
+        let token = localStorage.getItem('token');
+        if (!token) {
+          // Si el token no está presente, redirige al usuario a la página de inicio de sesión
+          window.location.href = '/';
+          return;
         }
 
-        fetchData();
-    }, []);
+
+        const [pedidosResponse, pedidosFinalizadosResponse, itemsPedidosResponse, empleadosResponse, horasEmpleadosResponse, configuracionesResponse] = await Promise.all([
+          axios.get(URI, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }),
+          axios.get(URI_PEDIDOS_FINALIZADOS, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }),
+          axios.get(URI_ITEMS_PEDIDOS, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }),
+          axios.get(URI_EMPLEADOS_REGISTRADOS, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }),
+          axios.get(URI_HORAS_REGISTRADAS, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }),
+          axios.get(URI_CONFIGURACIONES, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }),
+        ]);
+        setTimeout(() => {
+          setPedidos(pedidosResponse.data);
+          setPedidosFinalizados(pedidosFinalizadosResponse.data);
+          setTotalPrecio(
+            pedidosFinalizadosResponse.data.reduce(
+              (accumulator, pedido) => accumulator + parseFloat(pedido.precio_pedido),
+              0
+            )
+          );
+          setItemsPedidos(
+            itemsPedidosResponse.data.reduce(
+              (accumulator, item) => accumulator + parseFloat(item.cantidad),
+              0
+            )
+          );
+
+          setEmpleados(empleadosResponse.data);
+          setHorasEmpleados(horasEmpleadosResponse.data.reduce(
+            (accumulator, hora) => accumulator + parseFloat(hora.total_horas),
+            0
+          ));
+
+          setTotalPagarEmpleados(horasEmpleadosResponse.data.reduce(
+            (accumulator, hora) => accumulator + parseFloat(hora.total_pagar),
+            0
+          ));
+
+          setNombreAdmin(configuracionesResponse.data[0].nombre_admin);
+          /*   setLoading(false); */
+        }, 2000);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setTimeout(() => {
+          setLoading(false);
+        }, 2000); // Aquí puedes ajustar el tiempo en milisegundos según tus necesidades
+      }
+    }
+
+    fetchData();
+  }, []);
 
 
-    return (
+  return (
 
-        <div className='cmp-container-markey-mostrarUsuarios'>
-            <CompHeader />
-            <CompNavegacionVertical />
-            <div className='cmp-screen-container'>
-                <div className="cmp-screen-container-title">
-                    <p className='cmp-markey-nombre-admin'>¡Bienvenido {nombre_admin}!</p>
-                </div>
-                <div className="cmp-container-cards">
-                    <div className="cmp-section-pedidos">
-                        <p className="cmp-title-card">Empleados</p>
-                        <p className="cmp-valor-card">{empleados.length}</p>
-                    </div>
-                    <div className="cmp-section-pedidos">
-                        <p className="cmp-title-card">Horas registradas</p>
-                        <p className="cmp-valor-card">{parseFloat(horasEmpleados).toFixed(2)}</p>
-                    </div>
-                    <div className="cmp-section-pedidos">
-                        <p className="cmp-title-card">Total pagado</p>
-                        <p className="cmp-valor-card">{totalPagarEmpleados.toLocaleString('es-CO', { style: 'currency', currency: 'COP' })}</p>
-                    </div>
-                    <div className="cmp-section-pedidos">
-                        <p className="cmp-title-card">Rendimiento</p>
-                        <p className="cmp-valor-card">El esperado...</p>
-                    </div>
-                </div>
-                <div className="cmp-container-cards">
-                    <div className="cmp-section-pedidos">
-                        <p className="cmp-title-card">Pedidos activos</p>
-                        <p className="cmp-valor-card">{pedidos.length}</p>
-                    </div>
-                    <div className="cmp-section-pedidos">
-                        <p className="cmp-title-card">Pedidos finalizados</p>
-                        <p className="cmp-valor-card">{pedidosFinalizados.length}</p>
-                    </div>
-                    <div className="cmp-section-pedidos">
-                        <p className="cmp-title-card">Productos vendidos</p>
-                        <p className="cmp-valor-card">{totalItemsPedidos}</p>
-                    </div>
-                    <div className="cmp-section-pedidos">
-                        <p className="cmp-title-card">Ingresos</p>
-                        <p className="cmp-valor-card">{totalPrecio.toLocaleString('es-CO', { style: 'currency', currency: 'COP' })}</p>
-                    </div>
-                </div>
-                <div className="cmp-container-graficas">
-                    <div className="cmp-markey-graficos">
-                    <p className='cmp-markey-title-graficos'>Ingresos mensuales</p>
-                        <BarsChart />
-                    </div>
-                    <div className="cmp-markey-graficos">
-                    <p className='cmp-markey-title-graficos'>Productos más solicitados</p>
-                        <PiesChart />
-                    </div>
-                </div>
-            </div>
+    <div className='cmp-container-markey-mostrarUsuarios'>
+      <CompHeader />
+      <CompNavegacionVertical />
+      {isLoading && (
+        <div className="loading-overlay">
+          <span className="title-cargando">Cargando su espacio de trabajo...</span>
+          <FaCircleNotch className="loading-icon" />
         </div>
+      )}
+      <div className='cmp-screen-container'>
+        <div className="cmp-screen-container-title">
+          <p className='cmp-markey-nombre-admin'>¡Bienvenido {nombre_admin}!</p>
+        </div>
+        <div className="cmp-container-cards">
+          <div className="cmp-section-pedidos">
+            <p className="cmp-title-card">Empleados</p>
+            <p className="cmp-valor-card">{empleados.length}</p>
+          </div>
+          <div className="cmp-section-pedidos">
+            <p className="cmp-title-card">Horas registradas</p>
+            <p className="cmp-valor-card">{parseFloat(horasEmpleados).toFixed(2)}</p>
+          </div>
+          <div className="cmp-section-pedidos">
+            <p className="cmp-title-card">Total pagado</p>
+            <p className="cmp-valor-card">{totalPagarEmpleados.toLocaleString('es-CO', { style: 'currency', currency: 'COP' })}</p>
+          </div>
+          <div className="cmp-section-pedidos">
+            <p className="cmp-title-card">Rendimiento</p>
+            <p className="cmp-valor-card">El esperado...</p>
+          </div>
+        </div>
+        <div className="cmp-container-cards">
+          <div className="cmp-section-pedidos">
+            <p className="cmp-title-card">Pedidos activos</p>
+            <p className="cmp-valor-card">{pedidos.length}</p>
+          </div>
+          <div className="cmp-section-pedidos">
+            <p className="cmp-title-card">Pedidos finalizados</p>
+            <p className="cmp-valor-card">{pedidosFinalizados.length}</p>
+          </div>
+          <div className="cmp-section-pedidos">
+            <p className="cmp-title-card">Productos vendidos</p>
+            <p className="cmp-valor-card">{totalItemsPedidos}</p>
+          </div>
+          <div className="cmp-section-pedidos">
+            <p className="cmp-title-card">Ingresos</p>
+            <p className="cmp-valor-card">{totalPrecio.toLocaleString('es-CO', { style: 'currency', currency: 'COP' })}</p>
+          </div>
+        </div>
+        <div className="cmp-container-graficas">
+          <div className="cmp-markey-graficos">
+            <p className='cmp-markey-title-graficos'>Ingresos mensuales</p>
+            <BarsChart />
+          </div>
+          <div className="cmp-markey-graficos">
+            <p className='cmp-markey-title-graficos'>Productos más solicitados</p>
+            <PiesChart />
+          </div>
+        </div>
+      </div>
+    </div>
 
-    )
+  )
 
 }
 
